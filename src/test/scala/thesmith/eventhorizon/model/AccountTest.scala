043ec57e5ef269c8729f4c9b9b9e24dfffb7ec9d
Ben Smith <ben@thesmith.co.uk>
2009-12-08 09:18:14 +0000

    still struggling with getting junit to play with datanucleus

--- src/test/scala/thesmith/eventhorizon/model/AccountTest.scala
+++ src/test/scala/thesmith/eventhorizon/model/AccountTest.scala
@@ -3,36 +3,49 @@ package thesmith.eventhorizon.model
 import org.junit._
 import Assert._
 
-import thesmith.eventhorizon._
-
-class AccountTest extends LocalDatastoreTestCase {
+@Test
+class AccountTest {
   val account: Account = new Account()
 
-  override def setUp() = {
-    super.setUp
+  @Before
+  def setUp() = {
+    SetupEnv.setUp()
     account.domain = "domain"
     account.personId = "thesmith"
     account.username = "thesmith"
     Model.persistAndFlush(account)
-    assertNotNull(account.id)
+    
+    val acc2 = new Account()
+    acc2.domain = "something"
+    acc2.personId = "blah"
+    acc2.username = "fjdklsfjdkls"
+    Model.persistAndFlush(acc2)
+    System.err.println("----------"+acc2.id)
   }
 
-  override def tearDown(): Unit = {
-    super.tearDown
-    Model.createQuery("delete from Account ac").executeUpdate()
+  @After
+  def tearDown(): Unit = {
+    Model.createQuery("delete from thesmith.eventhorizon.model.Account").executeUpdate()
+    Model.close()
+    SetupEnv.tearDown()
   }
 
+  @Test
   def testShouldPersistAccount() = {
+    System.err.println("------------"+account.id)
     assertNotNull(account.id)
   }
 
+  @Test
   def testShouldFindAccount() = {
+    assertNotNull(account.id)
     val acc = Model.find(classOf[Account], account.id)
     assertNotNull(acc)
     assertFalse(acc.isEmpty)
     assertEquals(account.username, acc.get.username)
   }
 
+  @Test
   def testShouldFindAccounts() = {
     val a = new Account()
     a.personId = "jlkldfs"
@@ -40,7 +53,7 @@ class AccountTest extends LocalDatastoreTestCase {
     a.username = "jfkdls"
     Model.persistAndFlush(a)
 
-    val accs = Model.createQuery[Account]("select e from Account e").getResultList
+    val accs = Model.createQuery[Account]("select e from thesmith.eventhorizon.model.Account e").getResultList
       
     assertNotNull(accs)
     assertFalse(accs.isEmpty)
