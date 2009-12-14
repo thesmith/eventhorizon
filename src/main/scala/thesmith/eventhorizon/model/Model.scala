27cecf8fcccc9e8b3ef16b9244d5db240a260465
Ben Smith <ben@thesmith.co.uk>
2009-12-14 23:04:54 +0000

    removed scala implementation

--- src/main/scala/thesmith/eventhorizon/model/Model.scala
+++
@@ -1,7 +0,0 @@
-package thesmith.eventhorizon.model
-
-import org.scala_libs.jpa.LocalEMF
-import net.liftweb.jpa.RequestVarEM
-
-object Model extends LocalEMF("transactions-optional") with RequestVarEM
-
