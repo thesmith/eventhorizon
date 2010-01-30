3600b9661fa6acc7b51b260558e3ffea3967b4ba
Ben Smith <ben@thesmith.co.uk>
2010-01-30 20:37:26 +0000

    a few js tweeks, fixed plain bootup

--- src/main/webapp/js/js.js
+++ src/main/webapp/js/js.js
@@ -1,50 +1,56 @@
-$(document).ready(
-    function() {
-      $(".previous").css("opacity", "0");
-      $(".next").css("opacity", "0");
+$(document).ready(function() {
+  $(".previous").css("opacity", "0");
+  $(".next").css("opacity", "0");
 
-      var url = document.location.toString();
-      if (url.match("#")) {
-        var urlArray = url.split("#");
-        var user = getUser(urlArray[0]);
-        var anchor = urlArray[1];
-        updatePage(user + '/now', anchor);
-      } else {
-        var pathArray = window.location.pathname.split("/").clean("");
-        var host = window.location.host
-        var protocol = window.location.protocol
+  var url = document.location.toString();
+  if (url.match("#")) {
+    var urlArray = url.split("#");
+    var user = getUser(urlArray[0]);
+    var anchor = urlArray[1];
+    updatePage(user + '/now', anchor);
+  } else {
+    var pathArray = window.location.pathname.split("/").clean("");
+    var host = window.location.host;
+    var protocol = window.location.protocol;
+    var user = pathArray[0];
 
-        if (pathArray.length == 7) {
-          var currentUrl = urlBase(protocol, host, pathArray[0]) + "/#/" + (pathArray.slice(1, pathArray.length).join("/"));
-          window.location.replace(currentUrl);
-        } else if (pathArray.length == 1) {
-          var currentUrl = urlBase(protocol, host, pathArray[0]) + "/#/" + urlDate(new Date());
-          window.location.replace(currentUrl);
-        }
-      }
-    });
+    if (pathArray.length == 7) {
+      var currentUrl = urlBase(protocol, host, user) + "/#/" + (pathArray.slice(1, pathArray.length).join("/"));
+      window.location.replace(currentUrl);
+    } else if (pathArray.length == 1) {
+      var dateAsUrl = urlDate(new Date());
+      var currentUrl = urlBase(protocol, host, user) + "/#/" + dateAsUrl;
+      window.location.replace(currentUrl);
+      updatePage(user + '/now', dateAsUrl);
+    }
+  }
+});
 
 function updatePage(urlAppend, from) {
   $.getJSON('/' + urlAppend + '?from=' + from, function(data) {
     var user = getUser(document.location.toString().split("#")[0]);
     var host = window.location.host
     var protocol = window.location.protocol
-    
-    $.each(data.statuses, function(i, status) {
-      eventhorizonDates[status.domain] = urlDate(new Date(status.created));
-      $("#" + status.domain + " .status").html(status.status).removeClass(
-          'yonks month week today').addClass(status.period);
-      $("#" + status.domain + " .previous a").attr("href", urlBase(protocol, host, user) 
-          + "/" + eventhorizonDates[status.domain] + "/" + status.domain + "/previous");
-      $("#" + status.domain + " .next a").attr("href", urlBase(protocol, host, user) 
-          + "/" + eventhorizonDates[status.domain] + "/" + status.domain + "/next");
-    });
-    eventhorizonFromDate = new Date(data.from);
-    $(".title_date").html(titleDate(eventhorizonFromDate));
-    $(".title_time").html(titleTime(eventhorizonFromDate));
-    
-    var currentUrl = urlBase(protocol, host, user) + "/#/" + urlDate(eventhorizonFromDate);
-    window.location.replace(currentUrl);
+
+    if (data.statuses) {
+      $.each(data.statuses, function(i, status) {
+        eventhorizonDates[status.domain] = urlDate(new Date(status.created));
+        $("#" + status.domain + " .status").html(status.status).removeClass('yonks month week today').addClass(
+            status.period);
+        $("#" + status.domain + " .previous a").attr("href",
+            urlBase(protocol, host, user) + "/" + eventhorizonDates[status.domain] + "/" + status.domain
+                + "/previous");
+        $("#" + status.domain + " .next a").attr("href",
+            urlBase(protocol, host, user) + "/" + eventhorizonDates[status.domain] + "/" + status.domain
+                + "/next");
+      });
+      eventhorizonFromDate = new Date(data.from);
+      $(".title_date").html(titleDate(eventhorizonFromDate));
+      $(".title_time").html(titleTime(eventhorizonFromDate));
+
+      var currentUrl = urlBase(protocol, host, user) + "/#/" + urlDate(eventhorizonFromDate);
+      window.location.replace(currentUrl);
+    }
   });
 }
 
